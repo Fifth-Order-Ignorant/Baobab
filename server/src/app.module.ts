543c3b57c7bd61632dd1642d883c66707aa80cce
Ethan Lam <ethan.lam@mail.utoronto.ca>
2021-06-04 21:34:24 -0400

    [FIF-4] Add login and jwt controllers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AuthController],
   providers: [AppService],
 })
 export class AppModule {}
