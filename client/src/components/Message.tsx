62f9273551387811f81cf2b58a07dfcbbf6be2a4
Jan Garong <jan.garong@mail.utoronto.ca>
2021-06-05 10:30:10 -0400

    [FIF-13] Add Message.tsx.

---
+++ client/src/components/Message.tsx
@@ -0,0 +1,35 @@
+import { Card, Typography } from 'antd';
+
+/**
+ * Required props for rendering a message.
+ */
+interface MessageProps {
+    author: String;
+    postTime: String;
+    content: String;
+}
+
+/**
+ * Renders the message component.
+ * The following parameters are passed as props:
+ * @param author Author's name (first and last).
+ * @param postTime How long a message was sent.
+ * @param content Message content.
+ */
+export default function Message(
+  props: MessageProps
+): JSX.Element {
+  return (
+  <Card>
+    <p>
+    <Typography.Text type="secondary">
+      {props.author} made a post
+      <span>
+      &nbsp;&nbsp;{props.postTime}
+      </span>
+    </Typography.Text>
+    </p>
+    <p>{props.content}</p>
+  </Card>
+  );
+}
