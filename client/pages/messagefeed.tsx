e35f0012f4d34fd934f534621652792d7261d0f9
Jan Garong <jan.garong@mail.utoronto.ca>
2021-06-05 20:30:04 -0400

    [FIF-13] Add docstring for MessageFeed page.

--- client/pages/messagefeed.tsx
+++ client/pages/messagefeed.tsx
@@ -2,6 +2,9 @@ import { MessageList } from "../src/components/MessageList";
 import sampleMessages from "../src/constants/SampleMessageList";
 import { Typography } from "antd";
 
+/**
+ * Renders the MessageFeed page.
+ */
 export default function MessageFeed(): JSX.Element{
     return(
         <div style={{backgroundColor: "#f0f0f0"}}>
