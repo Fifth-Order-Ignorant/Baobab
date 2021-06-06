5c9ccb9853b9610bf29675c55c23f10d03ccaa95
mio <knightopress@gmail.com>
2021-06-06 15:52:53 -0400

    [FIF-4] Implement trivial logout.

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new YupValidationPipe());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
