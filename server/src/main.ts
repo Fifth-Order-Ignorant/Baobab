1d287b16a309137a97ec2189e9487f03175b3b55
mio <knightopress@gmail.com>
2021-06-07 14:01:44 -0400

    [FIF-4] Implement cookie removal correctly.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { YupValidationPipe } from './controllers/yup.pipe';
 import { AppModule } from './modules/app.module';
 import * as cookieParser from 'cookie-parser';
+import { CustomExceptionsFilter } from './controllers/unauthorized.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CustomExceptionsFilter(httpAdapter));
+
   app.useGlobalPipes(new YupValidationPipe());
   app.use(cookieParser());
   await app.listen(3001);
