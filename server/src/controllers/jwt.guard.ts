4207647739aafe38d2c2bbcfd3aabd4c2b6072fd
mio <knightopress@gmail.com>
2021-06-07 13:46:23 -0400

    [FIF-4] Rollback incorrect JwtAuthGuard.

--- server/src/controllers/jwt.guard.ts
+++ server/src/controllers/jwt.guard.ts
@@ -25,32 +25,26 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     const jwt = extractJwtFromCookie(request);
     const integrityString = request.cookies['SESSION_INT'];
 
-    if (
-      !super.canActivate(context) ||
-      !this._authService.verifyJwt(jwt, integrityString)
-    ) {
-      if (jwt) {
-        // remove invalid cookies
-        response.clearCookie('SESSION_JWT');
-        response.clearCookie('SESSION_INT');
+    if (jwt) {
+      if (!this._authService.verifyJwt(jwt, integrityString)) {
+        throw new UnauthorizedException();
       }
-      throw new UnauthorizedException();
+
+      // handle auto renewal of session token
+      const renewed = this._authService.renew(jwt);
+      if (renewed) {
+        response.cookie('SESSION_JWT', renewed.jwt, {
+          secure: this._configService.get<boolean>('production'),
+          sameSite: 'lax',
+        });
+        response.cookie('SESSION_INT', renewed.integrityString, {
+          httpOnly: true,
+          secure: this._configService.get<boolean>('production'),
+          sameSite: 'lax',
+        });
+      }
     }
 
-    // handle auto renewal of session token
-    const renewed = this._authService.renew(jwt);
-    if (renewed) {
-      response.cookie('SESSION_JWT', renewed.jwt, {
-        secure: this._configService.get<boolean>('production'),
-        sameSite: 'lax',
-      });
-      response.cookie('SESSION_INT', renewed.integrityString, {
-        httpOnly: true,
-        secure: this._configService.get<boolean>('production'),
-        sameSite: 'lax',
-      });
-    }
-
-    return true;
+    return super.canActivate(context);
   }
 }
