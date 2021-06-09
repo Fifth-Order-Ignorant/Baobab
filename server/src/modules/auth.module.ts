5c91e548b6292d627ffac64d8d17f2b787d1a958
Maryam Gohargani <60069680+magnesium4@users.noreply.github.com>
2021-06-09 17:43:57 -0400

    FIF-20 Create Message Module. Fix bug with JwtStrategy. Fix bug with parentID in MessageRequest.

--- server/src/modules/auth.module.ts
+++ server/src/modules/auth.module.ts
@@ -2,6 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from '../controllers/auth.controller';
+import { JwtStrategy } from '../controllers/jwt.strategy';
 import { UserModule } from './user.module';
 import { AuthService } from '../services/auth.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -20,7 +21,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     forwardRef(() => UserModule),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
