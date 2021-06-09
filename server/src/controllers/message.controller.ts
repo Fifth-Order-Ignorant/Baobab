6f47b1b190efe229f99f942eade8af14cc5769db
Maryam Gohargani <60069680+magnesium4@users.noreply.github.com>
2021-06-09 19:10:14 -0400

    FIF-20 Add tests for the messaging system. Add error checking for bad request in messaging.

--- server/src/controllers/message.controller.ts
+++ server/src/controllers/message.controller.ts
@@ -6,6 +6,7 @@ import {
   Res,
   UseGuards,
   Req,
+  BadRequestException,
 } from '@nestjs/common';
 import { MessageService } from '../services/message.service';
 import { Response } from 'express';
@@ -17,18 +18,24 @@ export class MessageController {
   constructor(private _messageService: MessageService) {}
 
   @UseGuards(JwtAuthGuard)
-  @Post('message')
+  @Post('create')
   createMessage(
     @Body() reqBody: MessageRequest,
     @Res({ passthrough: true }) res: Response,
     @Req() req,
   ) {
     const today = new Date();
+    const parent = this._messageService.getParentMessage(reqBody.parentID);
+    if (!parent) {
+      throw new BadRequestException({
+        errors: [],
+      });
+    }
     const message = this._messageService.createMessage(
       req.user.id,
       reqBody.content,
       today,
-      reqBody.parentID,
+      parent,
     );
     if (!message) {
       throw new InternalServerErrorException({
