8113526db0e085e55d21948197725753723a7e30
Jan Garong <jan.garong@mail.utoronto.ca>
2021-06-09 00:53:39 -0400

    [FIF-17] Add profile layout to the page.

--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -1,4 +1,9 @@
 import { About } from "../src/components/About";
+import { MessageList } from "../src/components/MessageList";
+import sampleMessages from "../src/constants/SampleMessageList";
+import { Avatar, Row, Col, Typography } from "antd";
+import styles from "../styles/Profile.module.css";
+import sampleProfile from "../src/constants/SampleProfile";
 
 /**
  * Renders the profile page.
@@ -6,7 +11,24 @@ import { About } from "../src/components/About";
 export default function Profile(): JSX.Element {
     return (
         <div>
-            <About name={"Hello World"} role={"Entrepeneur"} aboutMe={"I like to read!"}/>
+            <div className={styles.body}>
+                <Row className={styles.header}>
+                    <Col>
+                        <Avatar size="large" className={styles.avatar} />
+                    </Col>
+                    <Col className={styles.about}>
+                        <About name={sampleProfile.name} role={sampleProfile.role} aboutMe={sampleProfile.aboutMe} />
+                    </Col>
+                </Row>
+                <Row justify="center">
+                    <Col className={styles.activity} span={24}>
+                        <Typography>
+                            <h2>Recent Activity:</h2>
+                        </Typography>
+                        <MessageList messagePropsList={sampleMessages} />
+                    </Col>
+                </Row>
+            </div>
         </div>
     );
 }
