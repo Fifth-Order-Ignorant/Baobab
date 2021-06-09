73f3680fac242e10360f434b2d464435aa448f2b
Ethan Lam <ethan.lam@mail.utoronto.ca>
2021-06-09 14:10:33 -0400

    FIF-7: fix linting error

--- server/src/dao/messages.ts
+++ server/src/dao/messages.ts
@@ -2,16 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { Message } from '../entities/message.entity';
 
 export interface MessageDAO {
-    createMessage(
-      userId: number,
-      content: string,
-      parent: Message
-    ): number;
-    getChilds(id: number): Message[];
-    getById(id: number): Message;
-    getParent(id: number): Message;
+  createMessage(userId: number, content: string, parent: Message): number;
+  getChilds(id: number): Message[];
+  getById(id: number): Message;
+  getParent(id: number): Message;
 }
-
 
 @Injectable()
 export class MessageInMemory implements MessageDAO {
@@ -25,23 +20,24 @@ export class MessageInMemory implements MessageDAO {
     this.messageCount = 0;
   }
 
-  public createMessage(userId: number,
+  public createMessage(
+    userId: number,
     content: string,
-    parent: Message): number {
-        let message: Message;
-        message = new Message(this.highestId, userId, content, parent)
-        this.messages.push(message);
-        this.highestId++;
-        
-        return this.highestId - 1;
-    }
+    parent: Message,
+  ): number {
+    const message = new Message(this.highestId, userId, content, parent);
+    this.messages.push(message);
+    this.highestId++;
+
+    return this.highestId - 1;
+  }
 
   public getById(id: number): Message {
     let message: Message;
     this.messages.forEach((element) => {
-        if (element.id === id) {
-            message = element;
-        } 
+      if (element.id === id) {
+        message = element;
+      }
     });
     return message;
   }
@@ -51,8 +47,6 @@ export class MessageInMemory implements MessageDAO {
   }
 
   public getParent(id: number): Message {
-      return this.getById(id).parent;
+    return this.getById(id).parent;
   }
 }
-
-
--- server/src/entities/message.entity.ts
+++ server/src/entities/message.entity.ts
@@ -1,42 +1,41 @@
 export class Message {
-    private _id: number;
-    private _userId: number;
-    private _content: string;
-    private _childs: Message[];
-    // If no parent, _parent is undefined.
-    private _parent: Message;
-  
-    public constructor(
-      id: number,
-      userId: number,
-      content: string,
-      parent: Message,
-    ) {
-      this._id = id;
-      this._userId = userId;
-      this._content = content;
-      this._parent = parent;
-      this._childs = [];
-    }
-  
-    get id(): number {
-      return this._id;
-    }
-  
-    get userID(): number {
-      return this._userId;
-    }
-  
-    get content(): string {
-      return this._content;
-    }
-  
-    get childs(): Message[] {
-      return this._childs;
-    }
+  private _id: number;
+  private _userId: number;
+  private _content: string;
+  private _childs: Message[];
+  // If no parent, _parent is undefined.
+  private _parent: Message;
 
-    get parent(): Message {
-        return this._parent;
-    }
+  public constructor(
+    id: number,
+    userId: number,
+    content: string,
+    parent: Message,
+  ) {
+    this._id = id;
+    this._userId = userId;
+    this._content = content;
+    this._parent = parent;
+    this._childs = [];
   }
-  
+
+  get id(): number {
+    return this._id;
+  }
+
+  get userID(): number {
+    return this._userId;
+  }
+
+  get content(): string {
+    return this._content;
+  }
+
+  get childs(): Message[] {
+    return this._childs;
+  }
+
+  get parent(): Message {
+    return this._parent;
+  }
+}
