ac3cda1a1798ff691f4d8cdf590b4e61311496cf
Maryam Gohargani <60069680+magnesium4@users.noreply.github.com>
2021-06-09 18:02:41 -0400

    FIF-20 Fix linting errors.

--- server/src/services/message.service.ts
+++ server/src/services/message.service.ts
@@ -4,7 +4,6 @@ import { Message } from '../entities/message.entity';
 
 @Injectable()
 export class MessageService {
-
   constructor(@Inject('MessageDAO') private _messageRepository: MessageDAO) {}
 
   createMessage(
@@ -13,7 +12,13 @@ export class MessageService {
     timestamp: Date,
     parentID: number,
   ): Message {
-    return this._messageRepository.getByID(this._messageRepository.createMessage(userID, content, timestamp, this._messageRepository.getByID(parentID)))
+    return this._messageRepository.getByID(
+      this._messageRepository.createMessage(
+        userID,
+        content,
+        timestamp,
+        this._messageRepository.getByID(parentID),
+      ),
+    );
   }
-
 }
