516785bf4d9c2761daf10c4f5ee68a6b575a07df
Akira Takaki <34190365+DigestedLime@users.noreply.github.com>
2021-06-10 13:38:28 -0400

    [FIF-22] Fetch Message API (#40)
    * [FIF-22] Add messages slicing to MessageDAO.
    
    * [FIF-22] Add pagination to messages.
    
    * [FIF-22] Formatted code.

--- server/src/services/message.service.ts
+++ server/src/services/message.service.ts
@@ -1,10 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { MessageDAO } from '../dao/messages';
 import { Message } from '../entities/message.entity';
+import { Profile } from '../entities/profile.entity';
+import { UserProfileDAO } from '../dao/userprofiles';
 
 @Injectable()
 export class MessageService {
-  constructor(@Inject('MessageDAO') private _messageRepository: MessageDAO) {}
+  constructor(
+    @Inject('UserProfileDAO') private _userRepository: UserProfileDAO,
+    @Inject('MessageDAO') private _messageRepository: MessageDAO,
+  ) {}
 
   createMessage(
     userID: number,
@@ -19,5 +24,19 @@ export class MessageService {
 
   getParentMessage(parentID: number): Message {
     return this._messageRepository.getByID(parentID);
+  }
+
+  getPaginatedMessages(
+    start: number,
+    end: number,
+  ): Record<string, string | number>[] {
+    const messages: Record<string, string | number>[] =
+      this._messageRepository.getMessages(start, end);
+    messages.forEach((element) => {
+      const id: number = element.author as number;
+      const profile: Profile = this._userRepository.getProfileByID(id);
+      element.author = profile.name;
+    });
+    return messages;
   }
 }
