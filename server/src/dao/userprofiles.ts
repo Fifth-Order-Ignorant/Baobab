24d497de6948aa8d9a424aaaf1c594b46d6bbfac
Ethan Lam <55158216+EthanLam1@users.noreply.github.com>
2021-06-10 00:13:05 -0400

    [FIF-3] Profile Integration (#33)
    * FIF-12 fix up profile entity, add role entity, change users to userprofiles
    
    * FIF-12 Fix linting
    
    * FIF-12 fix Id -> ID for consistency
    
    * FIF-12 fix Id -> ID for consistency electric boogaloo

---
+++ server/src/dao/userprofiles.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { Profile } from '../entities/profile.entity';
+
+export interface UserProfileDAO {
+  addUserProfile(
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+  ): number;
+  getUserProfileCount(): number;
+  getUserByID(id: number): User;
+  getUserByEmail(email: string): User;
+  getProfileByID(id: number): Profile;
+}
+
+@Injectable()
+export class UserProfilesInMemory implements UserProfileDAO {
+  users: User[];
+  profiles: Profile[];
+  highestID: number;
+  userProfileCount: number;
+
+  public constructor() {
+    this.users = [];
+    this.profiles = [];
+    this.highestID = 0;
+    this.userProfileCount = 0;
+  }
+
+  public addUserProfile(
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+  ): number {
+    const newUser = new User(
+      this.highestID,
+      firstName,
+      lastName,
+      email,
+      password,
+    );
+    this.users.push(newUser);
+    const newProfile = new Profile(this.highestID, firstName + ' ' + lastName);
+    this.profiles.push(newProfile);
+    this.highestID++;
+    this.userProfileCount++;
+    return this.highestID - 1;
+  }
+
+  public getUserProfileCount() {
+    return this.userProfileCount;
+  }
+
+  public getUserByID(id: number): User {
+    let user: User;
+
+    this.users.forEach((element) => {
+      if (element.id === id) {
+        user = element;
+      }
+    });
+    return user;
+  }
+
+  public getUserByEmail(email: string): User {
+    let user: User;
+
+    this.users.forEach((element) => {
+      if (element.email === email) {
+        user = element;
+      }
+    });
+    return user;
+  }
+
+  public getProfileByID(id: number): Profile {
+    let profile: Profile;
+
+    this.profiles.forEach((element) => {
+      if (element.id === id) {
+        profile = element;
+      }
+    });
+    return profile;
+  }
+}
