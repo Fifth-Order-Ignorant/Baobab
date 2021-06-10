60323a357b96fc71a6dad1844cc6ee8e5a1eb59b
Maryam Gohargani <60069680+magnesium4@users.noreply.github.com>
2021-06-10 13:47:05 -0400

    FIF-20 fixed a bug with when message does not have parent id

--- server/src/controllers/message.controller.ts
+++ server/src/controllers/message.controller.ts
@@ -12,6 +12,7 @@ import { MessageService } from '../services/message.service';
 import { Response } from 'express';
 import { MessageRequest } from 'baobab-common';
 import { JwtAuthGuard } from './jwt.guard';
+import { Message } from '../entities/message.entity';
 
 @Controller('message')
 export class MessageController {
@@ -25,11 +26,16 @@ export class MessageController {
     @Req() req,
   ) {
     const today = new Date();
-    const parent = this._messageService.getParentMessage(reqBody.parentID);
-    if (!parent) {
-      throw new BadRequestException({
-        errors: [],
-      });
+    let parent: Message;
+    if (reqBody.parentID == -1) {
+      parent = undefined;
+    } else {
+      parent = this._messageService.getParentMessage(reqBody.parentID);
+      if (!parent) {
+        throw new BadRequestException({
+          errors: [],
+        });
+      }
     }
     const message = this._messageService.createMessage(
       req.user.id,
