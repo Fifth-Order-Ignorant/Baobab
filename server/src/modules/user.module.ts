86481999eae631d421b33a56090b81172b16607a
Akira Takaki <34190365+DigestedLime@users.noreply.github.com>
2021-06-10 22:13:15 -0400

    [FIF-22] Fetch Message API (#50)
    * [FIF-22] Add messages slicing to MessageDAO.
    
    * [FIF-22] Add pagination to messages.
    
    * [FIF-22] Formatted code.
    
    * [FIF-22] Add message pagination requests.
    
    * [FIF-22] Fix pagination requests.
    
    * [FIF-22] Format code.
    
    * [FIF-31] Work around circular dependency between auth and user modules. (#49)
    
    Co-authored-by: miosenpai <knightopress@gmail.com>

--- server/src/modules/user.module.ts
+++ server/src/modules/user.module.ts
@@ -1,11 +1,9 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserController } from '../controllers/user.controller';
 import { UserService } from '../services/user.service';
 import { UserProfilesInMemory } from '../dao/userprofiles';
-import { AuthModule } from './auth.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [
     { provide: 'UserDAO', useClass: UserProfilesInMemory },
