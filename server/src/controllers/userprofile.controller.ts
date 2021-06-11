92f709727edc31729435bd421e2bfd969ae6b744
Maryam Gohargani <60069680+magnesium4@users.noreply.github.com>
2021-06-11 15:01:54 -0400

    [FIF-21] Profile Editing API (#65)
    * FIF-21 Add get/post request & backend code for editing profiles
    
    * FIF-21 Format code with linter
    
    * FIF-21 Add tests for profile editing suite
    
    * FIF-21 Format code with linter
    
    * FIF-21 Separate end points for changing different parts of the profile. Add tests to reflect these changes.
    
    * FIF-21 Format code with linter.
    
    * FIF-21 Fix circular dependency of Jwt and profile editing

--- server/src/controllers/userprofile.controller.ts
+++ server/src/controllers/userprofile.controller.ts
@@ -3,45 +3,36 @@ import {
   Body,
   Controller,
   Get,
-  UseGuards,
   Post,
   Res,
-  Req,
 } from '@nestjs/common';
 import { UserProfileService } from '../services/userprofile.service';
 import { Response } from 'express';
 import {
   RegisterRequest,
   ProfilePaginationRequest,
-  EditNameRequest,
-  EditJobRequest,
-  EditBioRequest,
 } from 'baobab-common';
-import { AuthService } from '../services/auth.service';
 import { ValidationError } from 'yup';
 import { ConfigService } from '@nestjs/config';
-import { JwtAuthGuard } from './jwt.guard';
 
 @Controller('user')
 export class UserProfileController {
   constructor(
     private _userProfileService: UserProfileService,
-    private _authService: AuthService,
     private _configService: ConfigService,
   ) {}
 
   @Post('register')
   register(
     @Body() reqBody: RegisterRequest,
-    @Res({ passthrough: true }) res: Response,
+    // @Res({ passthrough: true }) res: Response,
   ) {
-    const userProfile = this._userProfileService.registerUser(
+    const user = this._userProfileService.registerUser(
       reqBody.firstName,
       reqBody.lastName,
       reqBody.email,
       reqBody.password,
     );
-    const user = userProfile ? userProfile[0] : null;
 
     if (!user) {
       throw new BadRequestException({
@@ -49,21 +40,11 @@ export class UserProfileController {
       });
     }
 
-    const { jwt, integrityString } = this._authService.genJwt(user.id);
-
-    res.cookie('SESSION_JWT', jwt, {
-      secure: this._configService.get<boolean>('production'),
-      sameSite: 'lax',
-    });
-
-    res.cookie('SESSION_INT', integrityString, {
-      httpOnly: true,
-      secure: this._configService.get<boolean>('production'),
-      sameSite: 'lax',
-    });
+    // redirect to automatically login (will work once we set up CORS later)
+    // res.redirect(307, 'http://localhost:3001/auth/login');
   }
 
-  @Get('pagination')
+  @Post('pagination')
   pagination(
     @Body() reqBody: ProfilePaginationRequest,
   ): Record<string, string>[] {
@@ -72,73 +53,5 @@ export class UserProfileController {
       reqBody.end,
     );
     return paginatedProfiles;
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Req() req) {
-    const id = req.user.id;
-    if (this._userProfileService.isValidProfile(id)) {
-      return this._userProfileService.getProfile(id);
-    } else {
-      throw new BadRequestException({
-        errors: [],
-      });
-    }
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Post('editname')
-  editName(
-    @Body() reqBody: EditNameRequest,
-    @Res({ passthrough: true }) res: Response,
-    @Req() req,
-  ): void {
-    const id = req.user.id;
-    if (this._userProfileService.isValidProfile(id)) {
-      this._userProfileService.editName(
-        id,
-        reqBody.firstName,
-        reqBody.lastName,
-      );
-    } else {
-      throw new BadRequestException({
-        errors: [],
-      });
-    }
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Post('editjob')
-  editJob(
-    @Body() reqBody: EditJobRequest,
-    @Res({ passthrough: true }) res: Response,
-    @Req() req,
-  ): void {
-    const id = req.user.id;
-    if (this._userProfileService.isValidProfile(id)) {
-      this._userProfileService.editJob(id, reqBody.jobTitle);
-    } else {
-      throw new BadRequestException({
-        errors: [],
-      });
-    }
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Post('editbio')
-  editBio(
-    @Body() reqBody: EditBioRequest,
-    @Res({ passthrough: true }) res: Response,
-    @Req() req,
-  ): void {
-    const id = req.user.id;
-    if (this._userProfileService.isValidProfile(id)) {
-      this._userProfileService.editBio(id, reqBody.bio);
-    } else {
-      throw new BadRequestException({
-        errors: [],
-      });
-    }
   }
 }
