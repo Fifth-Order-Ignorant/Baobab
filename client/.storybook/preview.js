f9c0964391c8bfec1b13fd8daed4471295f89c88
mio <knightopress@gmail.com>
2021-06-11 10:59:02 -0400

    [FIF-30] Add documentation generator to backend.

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -8,5 +8,5 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  viewMode: 'docs'
+  viewMode: "docs"
 }
--- common/src/requests/RegisterRequest.dto.ts
+++ common/src/requests/RegisterRequest.dto.ts
@@ -1,9 +1,21 @@
 import { object, SchemaOf, string } from 'yup';
 
 export class RegisterRequest {
+  /**
+   * First name of the user.
+   */
   firstName!: string;
+  /**
+   * Last name of the user.
+   */
   lastName!: string;
+  /**
+   * Email of the user.
+   */
   email!: string;
+  /**
+   * Password of the user.
+   */
   password!: string;
 }
 
@@ -11,5 +23,5 @@ export const RegisterRequestSchema: SchemaOf<RegisterRequest> = object({
   firstName: string().required(),
   lastName: string().required(),
   email: string().required().email(),
-  password: string().required()
-})
+  password: string().required(),
+});
