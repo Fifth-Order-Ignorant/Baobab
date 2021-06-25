dfa09c2f238a5487076ad27ca456b0988672c71a
Jan Garong <jan.garong@mail.utoronto.ca>
2021-06-24 22:25:16 -0400

    [FIF-54] Add profile tabs to the page.

--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -1,8 +1,9 @@
-import { About } from '../src/components/About';
 import { PostList } from '../src/components/PostList';
 import samplePosts from '../src/constants/SamplePostList';
+import { Tabs } from 'antd';
 import { Avatar, Row, Col, Typography } from 'antd';
 import styles from '../styles/Profile.module.css';
+import ProfileLayout from '../src/components/ProfileLayout';
 
 /**
  * Renders the profile page.
@@ -11,21 +12,32 @@ export default function Profile(): JSX.Element {
   return (
     <div>
       <div className={styles.body}>
-        <Row className={styles.header}>
-          <Col>
-            <Avatar size="large" className={styles.avatar} />
+        <Row>
+          <Col span={2} />
+          <Col span={4}>
+            <Avatar className={styles.avatar} />
           </Col>
-          <Col className={styles.about}>
-            <About />
+          <Col span={1} />
+          <Col className={styles.tabContents} span={13}>
+            <Tabs defaultActiveKey="profile" type="card">
+              <Tabs.TabPane tab="Profile" key="profile">
+                <ProfileLayout>
+                  <div />
+                </ProfileLayout>
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Activity" key="activity">
+                <ProfileLayout>
+                  <div>
+                    <Typography>
+                      <h2>Recent Activity:</h2>
+                    </Typography>
+                    <PostList postPropsList={samplePosts} />
+                  </div>
+                </ProfileLayout>
+              </Tabs.TabPane>
+            </Tabs>
           </Col>
-        </Row>
-        <Row justify="center">
-          <Col className={styles.activity} span={24}>
-            <Typography>
-              <h2>Recent Activity:</h2>
-            </Typography>
-            <PostList postPropsList={samplePosts} />
-          </Col>
+          <Col span={2} />
         </Row>
       </div>
     </div>
