1907d82217ba07f8eb409734dfaf5b41c3d6e398
xiaca <catherine.xia@mail.utoronto.ca>
2021-06-25 21:59:14 -0400

    [FIF-49] Change profile layout

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -1,9 +1,10 @@
 import { About } from '../../src/components/About';
 import { PostList } from '../../src/components/PostList';
 import sampleMessages from '../../src/constants/SamplePostList';
-import { Avatar, Row, Col, Typography } from 'antd';
+import { Avatar, Row, Col, Typography, Tabs } from 'antd';
 import styles from '../../styles/Profile.module.css';
 import { useRouter } from 'next/router';
+import samplePosts from '../../src/constants/SamplePostList';
 
 /**
  * Renders the profile page.
@@ -17,21 +18,32 @@ export default function Profile(): JSX.Element {
   return (
     <div>
       <div className={styles.body}>
-        <Row className={styles.header}>
-          <Col>
-            <Avatar size="large" className={styles.avatar} />
+        <Row>
+          <Col span={2} />
+          <Col span={4}>
+            <Avatar className={styles.avatar} />
           </Col>
-          <Col className={styles.about}>
-            <About id={id()}/>
+          <Col span={1} />
+          <Col className={styles.tabContents} span={13}>
+            <Tabs defaultActiveKey="profile" type="card">
+              <Tabs.TabPane tab="Profile" key="profile">
+                <Row>
+                  <Col>
+                    <About id={ id()} />
+                  </Col>
+                </Row>
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Activity" key="activity">
+                <div>
+                  <Typography>
+                    <h2>Recent Activity:</h2>
+                  </Typography>
+                  <PostList postPropsList={samplePosts} />
+                </div>
+              </Tabs.TabPane>
+            </Tabs>
           </Col>
-        </Row>
-        <Row justify="center">
-          <Col className={styles.activity} span={24}>
-            <Typography>
-              <h2>Recent Activity:</h2>
-            </Typography>
-            <PostList postPropsList={sampleMessages}  />
-          </Col>
+          <Col span={2} />
         </Row>
       </div>
     </div>
