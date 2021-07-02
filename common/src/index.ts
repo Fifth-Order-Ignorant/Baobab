db994972e2c04bbcd08b86e1fd3e062e492edf01
Jan Garong <42362229+jangarong@users.noreply.github.com>
2021-07-02 13:27:24 -0400

    [FIF-38] User Replies (#126)
    * [FIF-53] Replace PostList with PostFeed.
    
    * [FIF-52] Grabbing Posts API (#117)
    
    * [FIF-52] Add new endpoint with testing and refactor previous endpoints slightly.
    
    * [FIF-52] Modify endpoint to only send replies.
    
    * [FIF-52] Add missing file. (#119)
    
    * [FIF-52] Add new endpoint with testing and refactor previous endpoints slightly.
    
    * [FIF-52] Modify endpoint to only send replies.
    
    * [FIF-52] Add the UserRepliesPaginationRequest type.
    
    * [FIF-53] Fetch replies from user with an id of 0.
    
    * [FIF-53] Add reply feed to activity tab.
    
    * [FIF-53] Remove profile.tsx and sample post list.
    
    * [FIF-71] fix user activity to show posts instead of replies
    
    Co-authored-by: Akira <akira.takaki@mail.utoronto.ca>
    Co-authored-by: Akira Takaki <34190365+DigestedLime@users.noreply.github.com>
    Co-authored-by: r-a-m-e-n <71615467+r-a-m-e-n@users.noreply.github.com>
    Co-authored-by: Maryam Gohargani <60069680+magnesium4@users.noreply.github.com>

--- common/src/index.ts
+++ common/src/index.ts
@@ -15,6 +15,8 @@ export * from "./requests/ProfileViewRequest.dto";
 export * from "./requests/RegisterRequest.dto";
 export * from "./requests/RepliesPaginationRequest.dto";
 export * from "./requests/RoleRequest.dto";
+export * from "./requests/UserPostsPaginationRequest.dto";
+export * from "./requests/UserRepliesPaginationRequest.dto";
 export * from "./responses/ErrorResponse";
 export * from "./responses/PostResponse";
 export * from "./responses/ProfileResponse";
