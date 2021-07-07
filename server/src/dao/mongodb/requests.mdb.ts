7046a5c16b292aa1ff2d0a6d4454f14566ed910a
Jan Garong <42362229+jangarong@users.noreply.github.com>
2021-07-06 23:05:43 -0400

    [FIF-97] Run linter on all workspaces.

--- server/src/dao/mongodb/requests.mdb.ts
+++ server/src/dao/mongodb/requests.mdb.ts
@@ -5,11 +5,7 @@ import { Role } from '../../entities/role.entity';
 import { Model } from 'mongoose';
 
 export class RequestMongoDAO implements RequestDAO {
-
-  constructor(
-    @InjectModel(Request.name) private _requests: Model<Request>
-  ) {
-  }
+  constructor(@InjectModel(Request.name) private _requests: Model<Request>) {}
 
   async createRequest(
     userId: number,
@@ -18,14 +14,13 @@ export class RequestMongoDAO implements RequestDAO {
     role: Role,
   ): Promise<number> {
     const id = await this._requests.countDocuments();
-    await this._requests.create(new Request(id, userId, timestamp, description, role));
+    await this._requests.create(
+      new Request(id, userId, timestamp, description, role),
+    );
     return id;
   }
 
-  async getById(
-    id: number
-  ): Promise<Request>{
-    return await this._requests
-    .findById(id);
+  async getById(id: number): Promise<Request> {
+    return await this._requests.findById(id);
   }
 }
