d636b2aa01bbcb90f56ef1b78bd7960fe2e65284
Jan Garong <42362229+jangarong@users.noreply.github.com>
2021-07-08 22:32:15 -0400

    [FIF-97] Add alias.

--- server/src/dao/mongodb/schemas/request.schema.ts
+++ server/src/dao/mongodb/schemas/request.schema.ts
@@ -12,23 +12,28 @@ export const RequestSchema = new Schema<Request>(
     },
     _userID: {
       type: Number,
+      alias: 'userID',
       required: true,
     },
     _status: {
       type: Number,
+      alias: 'status',
       enum: Object.values(RequestStatus),
       required: true,
     },
     _timestamp: {
       type: Date,
+      alias: 'timestamp',
       required: true,
     },
     _description: {
       type: String,
+      alias: 'description',
       required: true,
     },
     _role: {
       type: String,
+      alias: 'role',
       enum: Object.values(Role),
       required: true,
     },
