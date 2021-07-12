be6f0252bd44a5f34daaf07f6e89cd08eb19916f
Maryam Gohargani <60069680+magnesium4@users.noreply.github.com>
2021-07-12 16:40:17 -0400

    [FIF-78] Update post backend to include tags when creating a post

--- common/src/requests/PostRequest.dto.ts
+++ common/src/requests/PostRequest.dto.ts
@@ -1,11 +1,13 @@
-import { object, SchemaOf, string, number } from 'yup';
+import { object, SchemaOf, string, number, array } from 'yup';
 
 export class PostRequest {
   content!: string;
   parentID!: number;
+  tags!: string[];
 }
 
 export const PostRequestSchema: SchemaOf<PostRequest> = object({
   content: string().required(),
   parentID: number().required(),
+  tags: array().of(string().required())
 });
