2462415ae84cf79b37a94661037bd2f705a6024b
mio <knightopress@gmail.com>
2021-07-22 13:14:51 -0400

    [FIF-113] Add AssignmentView to Storybook.

--- client/src/components/AssignmentView.tsx
+++ client/src/components/AssignmentView.tsx
@@ -2,22 +2,28 @@ import { AssignmentResponse } from 'baobab-common';
 import { Descriptions, Typography } from 'antd';
 
 type AssignmentViewProps = {
+  /**
+   * Data of assignment to be displayed.
+   */
   assignment: AssignmentResponse;
 };
 
+/**
+ * Component for displaying an individual assignment.
+ */
 function AssignmentView({ assignment }: AssignmentViewProps): JSX.Element {
   return (
     <Descriptions title={assignment.name} bordered column={2}>
-      <Descriptions.Item label="Max Mark">
+      <Descriptions.Item label="Max Mark" span={assignment.filename ? 1 : 2}>
         {assignment.maxMark}
       </Descriptions.Item>
-      <Descriptions.Item label="File">
-        {assignment.filename && (
+      {assignment.filename && (
+        <Descriptions.Item label="File">
           <Typography.Link href={`/api/assignment/file/${assignment.id}`}>
             {assignment.filename}
           </Typography.Link>
-        )}
-      </Descriptions.Item>
+        </Descriptions.Item>
+      )}
       <Descriptions.Item label="Description" span={2}>
         {assignment.description}
       </Descriptions.Item>
