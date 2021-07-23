00759543eb8259b0f85d8a68b5ac1eaa60546c1b
mio <knightopress@gmail.com>
2021-07-23 16:47:50 -0400

    [FIF-122] Remove duplicate role guard.

--- server/src/controllers/roles.guard.ts
+++ server/src/controllers/roles.guard.ts
@@ -14,7 +14,7 @@ export class RolesGuard implements CanActivate {
       context.getClass(),
     ]);
 
-    if (!requiredRoles || requiredRoles.length === 0) {
+    if (requiredRoles.length === 0) {
       return true;
     }
 
--- server/src/modules/request.module.ts
+++ server/src/modules/request.module.ts
@@ -3,16 +3,10 @@ import { UserProfileService } from '../services/userprofile.service';
 import { RequestController } from '../controllers/request.controller';
 import { RequestService } from '../services/request.service';
 import { AuthModule } from './auth.module';
-import { RolesGuard } from '../controllers/roles.guard';
-import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [AuthModule],
   controllers: [RequestController],
-  providers: [RequestService, UserProfileService,{
-    provide: APP_GUARD,
-    useClass: RolesGuard,
-  },
-  ],
+  providers: [RequestService, UserProfileService],
 })
 export class RequestModule {}
