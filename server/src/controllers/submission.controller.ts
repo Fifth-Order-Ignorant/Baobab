fc675fee624e04fbf27cc002f9c8c0c5e4dad9d6
Jan Garong <42362229+jangarong@users.noreply.github.com>
2021-07-24 11:39:35 -0400

    [FIF-114] Add filename to response. (#211)
    * [FIF-114] Add filename to response.
    
    * [FIF-114] Apply linter.

--- server/src/controllers/submission.controller.ts
+++ server/src/controllers/submission.controller.ts
@@ -71,6 +71,7 @@ export class SubmissionController {
       timestamp: submission.timestamp.toString(),
       mark: submission.mark,
       feedback: submission.feedback,
+      filename: submission.file.storedName,
     };
   }
 
@@ -98,6 +99,7 @@ export class SubmissionController {
         timestamp: submission.timestamp.toString(),
         mark: submission.mark,
         feedback: submission.feedback,
+        filename: submission.file.storedName,
       });
     }
     return subRes;
