cf6ca3dadda1ace7593b79d1b377ae961c88b7ad
Jan Garong <42362229+jangarong@users.noreply.github.com>
2021-07-27 09:34:01 -0400

    [FIF-117] Frontend for Mentor Assignment Page View (#213)
    * [FIF-117] Add submission component.
    
    * [FIF-117] Add submission list.
    
    * [FIF-117] Remove Submission component.
    
    * [FIF-117] Remove filename.
    
    * [FIF-117] Replace list with table pagination.
    
    * [FIF-117] Increase pageSize.
    
    * [FIF-117] Add role permissions in assignment page.
    
    * [FIF-117] Remove dependency to server role entity.
    
    * [FIF-117] Fix spaces.
    
    * [FIF-117] Fix linting issues.
    
    * [FIF-117] Fix Download link not showing.
    
    * [FIF-117] Allow admin to see submission pagination

---
+++ client/src/stories/SubmissionTable.stories.tsx
@@ -0,0 +1,37 @@
+import { SubmissionTable } from '../components/SubmissionTable';
+import { Meta } from '@storybook/react';
+
+export default {
+  title: 'SubmissionTable',
+  component: SubmissionTable,
+} as Meta;
+
+const dataSource = [
+  {
+    name: 'Jim Jim',
+    assignmentId: 0,
+    timestamp: Date().toString(),
+    feedback: 'Good work! A 10% improvement from your last test!',
+    mark: 10,
+    id: 0,
+  },
+  {
+    name: 'Tim Tim',
+    assignmentId: 0,
+    timestamp: Date().toString(),
+    feedback: 'Perfect!',
+    mark: 100,
+    id: 1,
+  },
+];
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export const Basic = (): JSX.Element => (
+  <SubmissionTable
+    fetchData={async () => {
+      return { data: dataSource, total: 2 };
+    }}
+    pageSize={2}
+    outOf={100}
+  />
+);
