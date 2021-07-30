80bde268291e57deff0b235a16140ef4dd5853bf
r-a-m-e-n <xiacatherine@yahoo.ca>
2021-07-30 19:26:32 -0400

    [FIF-125] Change form style

--- client/src/components/SubmissionTable.tsx
+++ client/src/components/SubmissionTable.tsx
@@ -8,7 +8,7 @@ import {
   SubmissionPaginationResponse,
   ErrorResponse,
 } from 'baobab-common';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import axios from 'axios';
 
@@ -26,35 +26,37 @@ export function SubmissionTable(props: SubmissionTableProps): JSX.Element {
   const [current, setCurrent] = useState(1);
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [open, setOpen] = useState<Array<boolean>>(new Array(data.length));
   const [edit, setEdit] = useState(-1);
+  const [mark, setMark] = useState<number>();
+  const [feedback, setFeedback] = useState<string>();
 
   const {
+    control,
+    handleSubmit,
     setError,
     formState: { errors, isSubmitting },
   } = useForm<UploadFeedbackRequest>({resolver: yupResolver(UploadFeedbackRequestSchema)});
 
-  const Update= async(subId: number, index: number) => {
-    let newMark = document
-    .getElementById('mark' + index)
-    ?.getAttribute('aria-valuenow') as unknown as number;
-    let newFeedback = document.getElementById("feedback" + 0)?.innerHTML as string;
-     const newData = data;
-     newData[index].mark = newMark;
-     newData[index].feedback = newFeedback;
+  const onSubmit: SubmitHandler<UploadFeedbackRequest>= async(prop) => {
+    prop.id = edit;
+    const newData = data;
+     newData[edit].mark = prop.mark;
+     newData[edit].feedback = prop.feedback;
      setData(newData);
      try {
-       await axios.patch('/api/submission/feedback', {id: subId, feedback: newFeedback, mark: newMark})
-       UpdateOpen(index, false);
-      } catch (error) {
-      const { errors } = error.response.data as ErrorResponse;
-      for (const error of errors) {
-        console.log(error);
-        setError(error.path as keyof UploadFeedbackRequest, {
-          message: error.message,
-        });
+       await axios.patch('/api/submission/feedback', prop)
+       UpdateOpen(edit);
+      } catch (e) {
+        if (axios.isAxiosError(e) && e.response) {
+          const { errors } = e.response.data as ErrorResponse;
+  
+          for (const error of errors) {
+            setError(error.path as keyof UploadFeedbackRequest, {
+              message: error.message,
+            });
+          }
+        }
       }
-    }
   }
 
   const Download = (id: number) => {
@@ -65,57 +67,16 @@ export function SubmissionTable(props: SubmissionTableProps): JSX.Element {
     );
   };
 
-  const UpdateOpen=(index: number, set: boolean) => {
-    let newArray = new Array(data.length);
-    for (let i = 0; i < data.length; i++) {
-      newArray[i] = false;
-    }
+  const UpdateOpen=(index: number) => {
     if (edit == -1) {
-    newArray[index] = true;
-    setOpen(newArray);
     setEdit(index);
     }
     else if (edit == index) {
-      setOpen(newArray);
       setEdit(-1);
     }
     else {
       setEdit(edit);
     }
-  }
-  
-  const EditMark=(maxMark: number, index: number) => {
-    return(
-      <Form.Item
-      name={"mark"}
-      validateStatus={errors.mark ? 'error' : ''}
-      help={errors.mark?.message}
-      >
-    <InputNumber
-      id={"mark" + index}
-      key={"mark"+index}
-      defaultValue={data[index].mark || 0}
-      min={0}
-      max={maxMark}
-      />
-      </Form.Item>
-    )
-  }
-
-  const EditFeedback=(index: number) => {
-    return(
-      <Form.Item
-      name={"feedback"}
-      validateStatus={errors.feedback ? 'error' : ''}
-      help={errors.feedback?.message}
-      >
-    <Input.TextArea
-      id={"feedback" + index}
-      key={"feedback"+index}
-      defaultValue={data[index].feedback}
-      />
-      </Form.Item>
-    )
   }
 
   /**
@@ -144,11 +105,6 @@ export function SubmissionTable(props: SubmissionTableProps): JSX.Element {
 
   useEffect(() => {
     fetch(current);
-    let newArray = new Array(data.length);
-    for (let i = 0; i < data.length; i++) {
-      newArray[i] = false;
-    }
-    setOpen(newArray);
   }, []);
 
   const columns = [
@@ -173,45 +129,76 @@ export function SubmissionTable(props: SubmissionTableProps): JSX.Element {
       dataIndex: 'mark',
       key: 'mark',
       editable: true,
-      render: (mark: number, info: AssignmentSubmissionResponse, index: number) => {
-        return open[index] ? (
-          EditMark(mark, index)
-        ):(
-          mark
-        )
-      },
     },
     {
       title: 'Feedback',
       dataIndex: 'feedback',
       key: 'feedback',
       editable: true,
-      render: (feedback: string, info: AssignmentSubmissionResponse, index: number) => {
-        return open[index] ? (
-          EditFeedback(index)
-        ):(
-          feedback
-        )
-      },
     },
     {
       title: 'Submit',
       dataIndex: 'submit',
       key: 'submit',
       render: (submit: string, info: AssignmentSubmissionResponse, index: number) => {
-        return open[index] ? (
-          <Button onClick={()=> Update(info.id, index)}>
-        Update
-      </Button>
-        ):(
-      <Button onClick={()=> UpdateOpen(index, true)}>
+        return(<Button onClick={()=> UpdateOpen(index)} disabled={edit!=-1}>
         Edit
-      </Button>
-        )
+      </Button>)
       },
     },
   ];
   return (
+    <div>
+    {edit != -1 && <Form onFinish={handleSubmit(onSubmit)}>
+      
+     <Controller
+      name={"id"}
+      control={control}
+      defaultValue={edit}
+      render={({field}) => (
+      <Form.Item
+      validateStatus={errors.mark ? 'error' : ''}
+      help={errors.mark?.message}
+      >
+    {false && <InputNumber size={"small"} {...field}
+      />}
+      </Form.Item>
+      )}
+      />
+      <h4>Feedback</h4>
+      <Controller
+      name={"mark"}
+      control={control}
+      defaultValue={data[edit].mark || 0}
+      render={({field}) => (
+      <Form.Item
+      label={"New Mark"}
+      validateStatus={errors.mark ? 'error' : ''}
+      help={errors.mark?.message}
+      >
+    <InputNumber min={0} max={props.outOf} id={'mark'} {...field}
+      />
+      </Form.Item>
+      )}
+      />
+      <Controller
+      name={"feedback"}
+      defaultValue={data[edit].feedback}
+      control={control}
+      render={({field}) => <Form.Item
+      label={"Feedback:"}
+        validateStatus={errors.feedback ? 'error' : ''}
+        help={errors.feedback?.message}
+      >
+        <Input.TextArea {...field} />
+      </Form.Item>
+      }/>
+      <Form.Item>
+            <Button type="primary" htmlType="submit" loading={isSubmitting}>
+        Update
+      </Button>
+            </Form.Item>
+    </Form>}
     <Table
       dataSource={data}
       columns={columns}
@@ -223,5 +210,6 @@ export function SubmissionTable(props: SubmissionTableProps): JSX.Element {
       }}
       onChange={handleTableChange}
     />
+    </div>
   );
 }
