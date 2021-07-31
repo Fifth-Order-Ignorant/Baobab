534610475917ce5beb06b219976b58b4a9e379e2
Jan Garong <42362229+jangarong@users.noreply.github.com>
2021-07-30 20:19:25 -0400

    [FIF-130] Add error page for createAssignment (#226)
    * [FIF-130] Add permissions to createAssignment.
    
    * [FIF-130] Apply linter.

--- client/pages/createAssignment.tsx
+++ client/pages/createAssignment.tsx
@@ -1,9 +1,25 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../src/providers/AuthProvider';
 import CreateAssignmentForm from '../src/components/CreateAssignmentForm';
+import ErrorPage from 'next/error';
 
 export default function CreateAssignment(): JSX.Element {
+  const authContext = useContext(AuthContext);
+  const isMentor = authContext
+    ? authContext.role.toString() === 'Mentor' ||
+      authContext.role.toString() === 'Admin'
+    : false;
+  if (isMentor) {
+    return (
+      <div style={{ backgroundColor: '#f0f0f0' }}>
+        <CreateAssignmentForm />
+      </div>
+    );
+  }
   return (
-    <div style={{ backgroundColor: '#f0f0f0' }}>
-      <CreateAssignmentForm />
-    </div>
+    <ErrorPage
+      statusCode={403}
+      title={'You do not have permissions to view this page'}
+    />
   );
 }
