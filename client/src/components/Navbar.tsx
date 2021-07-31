0432637dd3150fa354f29cf98181527214496f4d
Ethan Lam <ethan.lam@mail.utoronto.ca>
2021-07-30 20:13:53 -0400

    FIF-121 fix navbar to fit our pages

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../../src/providers/AuthProvider';
 import { Col, Menu, Row, Typography } from 'antd';
 import AuthComponent from './AuthComponent';
 import Link from 'next/link';
@@ -6,6 +8,11 @@ import Link from 'next/link';
  * Navigation bar component.
  */
 function Navbar(): JSX.Element {
+  const authContext = useContext(AuthContext);
+  const isMentor = authContext
+    ? authContext.role.toString() === 'Mentor' : false;
+  const isAdmin = authContext
+    ? authContext.role.toString() === 'Admin' : false;
   return (
     <Row justify="space-between" align="middle">
       <Col>
@@ -23,11 +30,22 @@ function Navbar(): JSX.Element {
           <Menu.Item key="education">
             <Link href="/assignments/">Education</Link>
           </Menu.Item>
-          <Menu.Item key="discussionBoard">Discussion Board</Menu.Item>
-          <Menu.Item key="posts">Posts</Menu.Item>
           <Menu.Item key="profiles">
             <Link href="/profiles/">Profiles</Link>
           </Menu.Item>
+          <Menu.Item key="createTeam">
+            <Link href="/createTeam/">Create Team</Link>
+          </Menu.Item>
+          {(isMentor || isAdmin) && (
+            <Menu.Item key="createAssignment">
+              <Link href="/createAssignment/">Create Assignment</Link>
+            </Menu.Item>
+          )}
+          {isAdmin && (
+            <Menu.Item key="adminRequest">
+              <Link href="/adminRequest/">Admin Requests</Link>
+            </Menu.Item>
+          )}
         </Menu>
       </Col>
       <Col>
